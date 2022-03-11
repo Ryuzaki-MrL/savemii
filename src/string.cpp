dd8ac031387ced2912b6a20886d7325149993e33
Xpl0itU <samuelcarballogarcia8@gmail.com>
2022-03-11 19:29:08 +0100

    Remove storage location hack

--- src/string.cpp
+++ src/string.cpp
@@ -1,27 +1,5 @@
 #include "string.hpp"
 
-char *replace_str(char *str, char *orig, char *rep)
-{
-  static char buffer[4096];
-  char *p;
-
-  if(!(p = strstr(str, orig)))  // Is 'orig' even in 'str'?
-    return str;
-
-  strncpy(buffer, str, p-str); // Copy characters from 'str' start to 'orig' st$
-  buffer[p-str] = '\0';
-
-  sprintf(buffer+(p-str), "%s%s", rep, p+strlen(orig));
-
-  return buffer;
-}
-
-bool StartsWith(const char *a, const char *b)
-{
-   if(strncmp(a, b, strlen(b)) == 0) return 1;
-   return 0;
-}
-
 string string_format(const string fmt, ...) {
     int size=100;
     string str;
--- src/string.hpp
+++ src/string.hpp
@@ -5,6 +5,4 @@ extern "C" {
 }
 using namespace std;
 
-char *replace_str(char *str, char *orig, char *rep);
-bool StartsWith(const char *a, const char *b);
 string string_format(const string fmt, ...);
