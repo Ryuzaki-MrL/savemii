979ddf007496d937edf35e0ec39f764f955494e0
Xpl0itU <samuelcarballogarcia8@gmail.com>
2022-08-18 16:17:58 +0200

    Revert "Fix checkEntry again"
    This reverts commit 44df1795e2f3885195273fd0a3eb07028e4f7428.

--- src/savemng.cpp
+++ src/savemng.cpp
@@ -117,12 +117,12 @@ int32_t loadTitleIcon(Title *title) {
 auto checkEntry(const char *fPath) -> int {
     FSStat stat;
     if (FSGetStat(__wut_devoptab_fs_client, &cmdBlk, fPath, &stat, FS_ERROR_FLAG_ALL) == FS_STATUS_OK && (stat.flags & FS_STAT_FILE))
-        return 1;
+        return 0;
 
     if (FSGetStat(__wut_devoptab_fs_client, &cmdBlk, fPath, &stat, FS_ERROR_FLAG_ALL) == FS_STATUS_OK && (stat.flags & FS_STAT_DIRECTORY))
         return 2;
 
-    return 0;
+    return 1;
 }
 
 static auto folderEmpty(const char *fPath) -> int {
